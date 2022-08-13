63e7f4a31062a95e480865f271228ffb8174ee65
Mikhail Abramov <mabramov@redhat.com>
2022-08-13 22:33:27 +0200

    Update page, add base routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouteOneComponent } from './route-one/route-one.component';
+import { RouteTwoComponent } from './route-two/route-two.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RouteThreeComponent } from './route-three/route-three.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RouteOneComponent,
+    RouteTwoComponent,
+    PageNotFoundComponent,
+    RouteThreeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      { path: 'route-one', component: RouteOneComponent },
+      { path: 'route-two', component: RouteTwoComponent },
+      { path: 'route-three', component: RouteThreeComponent },
+      { path: '', redirectTo: '/route-one', pathMatch: 'full' },
+      { path: '**', component: PageNotFoundComponent }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/route-three/route-three.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RouteThreeComponent } from './route-three.component';
+
+describe('RouteThreeComponent', () => {
+  let component: RouteThreeComponent;
+  let fixture: ComponentFixture<RouteThreeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RouteThreeComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RouteThreeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
