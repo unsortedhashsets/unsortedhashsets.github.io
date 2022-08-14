3fab505f0a62394fed4ab5d073e71d0c271e2d88
Mikhail Abramov <mabramov@redhat.com>
2022-08-14 18:18:09 +0200

    Update init page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ParticlesConfig } from 'src/app/particles-config';
+
+declare let particlesJS: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+  public ngOnInit(): void {
+    this.invokeParticles();
+  }
+
   title = 'business-card-site';
+
+  public invokeParticles(): void {
+    particlesJS('particles-js', ParticlesConfig, function () { });
+  }
 }
