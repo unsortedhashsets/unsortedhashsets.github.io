1ab2ea49f34bc2107b8aabdca494f429e080dbbb
Mikhail Abramov <mabramov@redhat.com>
2023-03-21 16:02:45 +0100

    Add window handlers

--- src/components/Window/index.tsx
+++ src/components/Window/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Draggable from "react-draggable";
 
 import "./Window.scss";
@@ -8,27 +9,44 @@ interface WindowProps {
   }
 
 const Window: React.FC<WindowProps> = ({children, title}) => {
-  
-    return (
-      <Draggable
-        handle=".tw-draggable">
-        <div className="tw-window">
-            <div className="tw-header">
-                <div className="tw-title  tw-draggable">
-                    { title }
+    const [window, setWindow] = useState(true);
+
+    const handleCloseWindow = () => {
+        setWindow(false);
+    };
+
+    const handleMaximizeWindow = () => {
+        console.log("Maximize");
+    };
+
+    const handleMinimizeWindow = () => {
+        console.log("Minimize");
+    };
+ 
+    if (window) {
+        return (
+            <Draggable
+                handle=".tw-draggable">
+                <div className="tw-window" id="tw-window">
+                    <div className="tw-header">
+                        <div className="tw-title  tw-draggable">
+                            { title }
+                        </div>
+                        <div className="tw-buttons">
+                            <button type="button" className="tw-button tw-minimize" onClick={handleMinimizeWindow}/>
+                            <button type="button" className="tw-button tw-maximize" onClick={handleMaximizeWindow}/>
+                            <button type="button" className="tw-button tw-close" onClick={handleCloseWindow}/>
+                        </div>
+                    </div>
+                    <div className="tw-body">
+                        { children }
+                    </div>
                 </div>
-                <div className="tw-buttons">
-                    <button type="button" className="tw-button tw-minimize" disabled/>
-                    <button type="button" className="tw-button tw-maximize" disabled/>
-                    <button type="button" className="tw-button tw-close" disabled/>
-                </div>
-            </div>
-            <div className="tw-body">
-                { children }
-            </div>
-        </div>
-      </Draggable>
-    );
+            </Draggable>
+        );
+    } else {
+        return null;
+    }
 };
  
 export default Window;
